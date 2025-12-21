45b77962e25a1a38d2de6135cb501ebe3194c2c1
x2Stiftz <omgth034@gmail.com>
2025-12-22 06:28:53 +0700

    feat(email): add email service module with various email sending functions

--- api/email.service-8WJwX3nu.mjs
+++
@@ -1,3 +0,0 @@
-import { a as sendOrderConfirmedEmail, c as sendPasswordResetEmail, i as sendOrderCompletedEmail, l as sendTestEmail, n as sendEmail, o as sendOrderPreparingEmail, r as sendOrderCancelledEmail, s as sendOrderReadyEmail, t as sendAccountLockedEmail, u as sendVerificationEmail } from "./index.mjs";
-
-export { sendTestEmail };
